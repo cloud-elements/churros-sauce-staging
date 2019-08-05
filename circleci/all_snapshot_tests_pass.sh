e2b123c0664565006fe9a0732ab658d3ecb8910e
Krishna Chaitanya Guttikodna <krishnachaitanya@Krishnas-MacBook-Pro-2.local>
2019-08-05 20:00:30 +0530

    updated config.yml to run on staging

--- circleci/all_snapshot_tests_pass.sh
+++ circleci/all_snapshot_tests_pass.sh
@@ -2,8 +2,8 @@
 
 set -xe
 
-TESTS_PASS_TAG="us-west-2_snapshot_tests_pass"
-DEPLOYED_TAG="us-west-2_snapshot_deployed"
+TESTS_PASS_TAG="us-west-2_staging_tests_pass"
+DEPLOYED_TAG="us-west-2_staging_deployed"
 
 # set the snapshot_tests_pass tag
 git clone https://github.com/cloud-elements/soba.git ~/soba
@@ -19,7 +19,7 @@ git checkout $VERSION_TAG
 git tag -a -f -m $VERSION_TAG $TESTS_PASS_TAG
 git push -f origin $TESTS_PASS_TAG
 
-~/slack-cli/src/slack chat send --channel '#qa-results' --color good --text "All *Snapshot Test* tests were successful, set tag ${TESTS_PASS_TAG} to ${VERSION_TAG} :tada:" -flds "{\"title\": \"Environment\", \"value\": \"Snapshot\", \"short\": true}" -a "{\"type\": \"button\", \"style\": \"primary\", \"text\": \"See results :mag_right:\", \"url\": \"${CIRCLE_BUILD_URL}\"}"
+~/slack-cli/src/slack chat send --channel '#staging-stop-and-fix' --color good --text "All *Staging Test* tests were successful, set tag ${TESTS_PASS_TAG} to ${VERSION_TAG} :tada:" -flds "{\"title\": \"Environment\", \"value\": \"Snapshot\", \"short\": true}" -a "{\"type\": \"button\", \"style\": \"primary\", \"text\": \"See results :mag_right:\", \"url\": \"${CIRCLE_BUILD_URL}\"}"
 
 popd
 
