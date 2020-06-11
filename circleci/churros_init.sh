986f2812382053a943af8bf6504d8b3fe06112b8
Krishna Chaitanya Guttikonda <krishnachaitanya@KrishnaChaitanyas-MacBook-Pro.local>
2020-06-11 17:35:49 +0530

    Adding Pipeline PArameters

--- circleci/churros_init.sh
+++ circleci/churros_init.sh
@@ -6,11 +6,11 @@ case "$1" in
     churros init --user 'churros@churros.com' --password 'elements1' --url 'https://snapshot.cloud-elements.com' --template ~/cloud-elements/churros-sauce/sauce.json --1pass --1pass-element all
   ;;
   staging|stg)
-    echo 'Initializing churros to run tests against staging...123'
+    echo 'Initializing churros to run tests against staging...'
     churros init --user 'churros@churros.com' --password 'elements1' --url 'https://staging.cloud-elements.com' --template ~/cloud-elements/churros-sauce-staging/sauce.json --1pass --1pass-element all
   ;;
   production|prod)
-    echo 'Initializing churros to run tests against staging...'
+    echo 'Initializing churros to run tests against production...'
     churros init --user 'churros@churros.com' --password 'elements1' --url 'https://api.cloud-elements.com' --template ~/cloud-elements/churros-sauce-staging/sauce.json --1pass --1pass-element all
   ;;
   *)
